fa8e2971a0d8610d1f3e9d16736340d9801b569b
Pablo Naranjo <pdnaranj@espol.edu.ec>
2025-06-11 15:21:52 -0500

    Creación de clases Estudiante y Profesor

---
+++ Taller05/src/Estudiante.java
@@ -0,0 +1,13 @@
+public class Estudiante extends PersonalAcademico{
+
+    public Estudiante(String usuario, String contraseña, String nombre, String apellido) {
+            super(usuario, contraseña, nombre, apellido);
+        }
+        
+    public void solicitaInscripcionCurso(Curso curso){
+        //LOGICA
+    }
+    public void realizarActividadSumativa(Curso curso, ActividadSumativa actividadSumativa){
+        //LOGICA
+    }
+}
