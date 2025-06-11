60fcfc74c666f51c4420ef3e8ce078811de33d71
Pablo Naranjo <pdnaranj@espol.edu.ec>
2025-06-11 16:10:44 -0500

    Actualización Estudiante

--- Taller05/src/Estudiante.java
+++ Taller05/src/Estudiante.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estudiante extends PersonalAcademico{
+
+    private List<Curso> cursosInscritos = new ArrayList();
+    private List<Curso> cursosEnEspera = new ArrayList();
 
     public Estudiante(String usuario, String contraseña, String nombre, String apellido) {
             super(usuario, contraseña, nombre, apellido);
